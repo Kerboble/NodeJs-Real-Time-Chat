a603e4c866b9f785875fa0ca10d3b5bec340a580
Kerboble <brennen.bogueca2023@gmail.com>
2024-02-20 15:03:56 -0800

    yup

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,14 +1,15 @@
 import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
+
 const ChatsPage = (props) => {
     return ( 
         <div style={{height: '100vh'}}> 
             <PrettyChatWindow 
-            projectId='
-            efeabda3-44d5-435d-b870-c64fe8005169'
+            projectId='efeabda3-44d5-435d-b870-c64fe8005169'
             username={props.user.username}
             secret={props.user.secret}
             style={{height: '100%'}}
+            renderChatList={(chatAppState) => {<Chatlist />}}
             />
         </div>
     )
